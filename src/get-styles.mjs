c7e617fb68fb559b97b65ab28abe9e1bd8553df7
Taylor Beseda <tbeseda@gmail.com>
2022-11-08 17:29:42 -0700

    getStyles uses generated styles from @architect/shared

--- src/get-styles.mjs
+++ src/get-styles.mjs
@@ -1,25 +1,24 @@
-import { join } from 'node:path'
-import { readFileSync } from 'node:fs'
+import { join } from 'node:path';
+import { readFileSync } from 'node:fs';
 
 // * utility for plugin consumers to grab a link tag or inline styles
 export default function getStyles() {
-  const tmpDir = new URL('../tmp', import.meta.url).pathname;
-  console.log('tmpDir', tmpDir)
+	const pluginOutputDir = join(
+		process.cwd(),
+		'node_modules',
+		'@architect',
+		'shared',
+		'enhance-styles',
+	);
+	const stylesFilePath = join(pluginOutputDir, 'styles.css');
 
-  const here = join(process.cwd(), 'tmp')
-  console.log('here', here)
+	let css;
+	try {
+		css = readFileSync(stylesFilePath, { encoding: 'utf-8' });
+	} catch (error) {
+		css = '/* enhance-styles placeholder */';
+	}
 
-  // TODO: check for styles in node_modules/@architect/enhance-styles
-
-  const checkPath = join(here, 'styles.css')
-  let css = '/* enhance-styles placeholder */'
-
-  try {
-    console.log('checkPath', checkPath)
-    css = readFileSync(checkPath, { encoding: 'utf-8' })
-  } catch (error) {
-    console.log('no css found')
-  }
-
-  return `<style>${css}</style>`
+	// TODO: optionally return <link> tag
+	return `<style>${css}</style>`;
 }
--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
 const { readFileSync, writeFileSync, mkdirSync } = require('node:fs')
-const path = require('node:path')
+const { join } = require('node:path')
 const defaultConfig = require('@enhance/styles/config.json')
 const TEMP_DIR_NAME = 'tmp'
 
 const hydrate = {
   async copy ({ arc, copy, inventory }) {
     const params = { arc, inventory }
+
     if (verify(params)) {
       const config = createConfig(params)
-      const { filePath } = await generateAndSave(config)
+      await generateAndSave(config)
 
-      // console.log(
-      //   'Saved CSS',
-      //   `to ${filePath}`,
-      // )
+      console.log('  Enhance Styles: CSS generated.')
 
       await copy({
         source: TEMP_DIR_NAME, // relative to the project root
         target: '@architect/shared/enhance-styles',
       })
     }
-  }
+  },
 }
 
 const sandbox = {
   async watcher (params) {
     if (verify(params)) {
-      const { fileName } = params
+      const { filename } = params
       const config = createConfig(params)
 
-      if (config.configPath && config.configPath === fileName) {
+      if (config.configPath && config.configPath === filename) {
+        console.log('  Enhance Styles: config changed, rebuilding.')
         await generateAndSave(config)
       }
     }
-  }
+  },
 }
 
 /**
@@ -47,8 +46,12 @@ function verify ({ arc, inventory }) {
   const hasShared = inventory.inv.shared?.src
   const hasConfig = arc['enhance-styles']?.length > 0
 
-  if (!hasShared) { console.warn('  Enhance Styles: Architect shared must be enabled') }
-  if (!hasConfig) { console.warn('  Enhance Styles: @enhance-styles config required') }
+  if (!hasShared) {
+    console.warn('  Enhance Styles: Architect shared must be enabled')
+  }
+  if (!hasConfig) {
+    console.warn('  Enhance Styles: @enhance-styles config required')
+  }
 
   return hasShared && hasConfig
 }
@@ -58,7 +61,7 @@ function verify ({ arc, inventory }) {
  * @param {object} params
  * @param {object} params.arc
  * @param {object} params.inventory
- * @returns {{cwd: string, fileName: string, configPath: null|string, stylesConfig: string,}}
+ * @returns {{configPath: null|string, cwd: string, fileName: string, stylesConfig: string}}
  */
 function createConfig ({ arc, inventory }) {
   const pluginConfig = Object.fromEntries(arc['enhance-styles'] || [])
@@ -70,10 +73,7 @@ function createConfig ({ arc, inventory }) {
   let configPath = null
 
   if (pluginConfig.config) {
-    configPath = path.join(
-      cwd,
-      pluginConfig.config,
-    )
+    configPath = join(cwd, pluginConfig.config)
     stylesConfig = readFileSync(configPath, 'utf-8')
   }
   else {
@@ -82,8 +82,8 @@ function createConfig ({ arc, inventory }) {
 
   return {
     cwd,
-    fileName,
     configPath,
+    fileName,
     stylesConfig,
   }
 }
@@ -91,23 +91,30 @@ function createConfig ({ arc, inventory }) {
 /**
  * Run enhance-styles and save result
  * @param {object} config
+ * @param {null|string} config.configPath styles config path, if provided
  * @param {string} config.cwd project working dir
  * @param {string} config.fileName output fileName .css
  * @param {string} config.stylesConfig stringified JSON Enhance Styles config
  * @param {boolean} write should write to disk
  * @returns {Promise<{generatedStyles: string, filePath: null|string}>}
  */
-async function generateAndSave ({ cwd, fileName, stylesConfig }, write = true) {
+async function generateAndSave (
+  { configPath, cwd, fileName, stylesConfig },
+  write = true,
+) {
   const { default: enhanceStyles } = await import('@enhance/styles')
   const generatedStyles = enhanceStyles(stylesConfig)
 
   let filePath = null
   if (write) {
-    const outputDir = path.join(cwd, TEMP_DIR_NAME)
-    filePath = path.join(outputDir, fileName)
+    const outputDir = join(cwd, TEMP_DIR_NAME)
+    filePath = join(outputDir, fileName)
 
     mkdirSync(outputDir, { recursive: true })
     writeFileSync(filePath, generatedStyles)
+    if (configPath) {
+      writeFileSync(join(outputDir, 'config.json'), stylesConfig)
+    }
   }
 
   return { generatedStyles, filePath }
