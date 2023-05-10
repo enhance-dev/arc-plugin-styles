9dc895db0ab850853a953444ca5d72ef1b3db527
kj <kj@begin.com>
2023-05-10 10:44:39 -0700

    Updates to latest enhance styles and updates tests

--- test/test.js
+++ test/test.js
@@ -2,10 +2,9 @@ const { join } = require('node:path')
 const { get } = require('tiny-json-http')
 const test = require('tape')
 const sandbox = require('@architect/sandbox')
-
+const xlQuery = '75em' // Configured in test/mock/styles-confoig.json
 const workingDirectory = join(process.cwd(), 'test', 'mock')
 const port = 6661
-const stylesConfigBase = 24 // configured in mock/enhance-styles.json
 const url = (path) => `http://localhost:${port}${path}`
 
 test(`Start Arc Sandbox in ${workingDirectory}`, async t => {
@@ -30,7 +29,8 @@ test('Sandbox working and styles resolving', async t => {
   const cssRequest = await get({ url: url('/enhance-styles.css') })
   const css = cssRequest.body
   t.ok(css, '.css available via HTTP')
-  t.ok(css.indexOf(`html {font-size: ${stylesConfigBase}px;}`) > 0, 'Styles configured.')
+  t.ok(css.indexOf(`@media only screen and (min-width:${xlQuery})`) > 0, 'Styles configured.')
+  // t.ok(css.indexOf(`html {font-size: ${stylesConfigBase}px;}`) > 0, 'Styles configured.')
 
   const styleGuideRequest = await get({ url: url('/_styleguide') })
   const styleGuide = styleGuideRequest.body
